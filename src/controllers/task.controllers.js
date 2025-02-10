ed99150ceb237559335ef9da2fefd6c150280e46
talhahzahid <talhazahid415@gmail.com>
2025-02-10 22:25:23 +0500

    GoNextProject

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,12 +1,13 @@
 import Usertask from "../models/task.models.js";
 
 const createTask = async (req, res) => {
-  const { tasktitle, description } = req.body;
-  if (!tasktitle) return res.json({ message: "Task Title is required" });
+  const { taskname, description } = req.body;
+  if (!taskname)
+    return res.status(400).json({ message: "Task Name is required" });
   if (!description)
-    return res.json({ message: "Task Description is required" });
+    return res.status(400).json({ message: "Task Description is required" });
   try {
-    const task = await Usertask.create({ tasktitle, description });
+    const task = await Usertask.create({ taskname, description });
     res.status(201).json({ message: "Task Adde Successfully", task });
   } catch (error) {
     res.status(500).json({ message: "Error Occurred" });
@@ -14,14 +15,29 @@ const createTask = async (req, res) => {
 };
 
 // remove task
-const removeTask = (req, res) => {};
+const removeTask = async (req, res) => {
+  const { id } = req.user;
+  try {
+    const remove = await Usertask.findByIdAndDelete(id);
+    res.status(200).json({ message: "Task Delete Successfully", remove });
+  } catch (error) {
+    res.status(500).json({ message: "Error Occurred" });
+  }
+};
 
 // edit task
 const updateTask = (req, res) => {};
 
 // All Task
-const allTask = (req, res) => {};
-
-
+const allTask = async (req, res) => {
+  const user = req.user;
+  if (!user) return res.status(400).json({ message: "Login First" });
+  try {
+    const task = await Usertask.find({});
+    res.status(200).json({ message: "All Task Fetch", task });
+  } catch (error) {
+    res.status(500).json({ message: "Error Occurred" });
+  }
+};
 
 export { createTask, removeTask, updateTask, allTask };
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { createTask } from "../controllers/task.controllers.js  ";
+import { authenticate } from "../middleware/authorize.middleware.js";
+import { allTask } from "../controllers/task.controllers.js";
 const router = express.Router();
 
-router.post("/createtask", createTask);
+router.post("/createtask", authenticate, createTask);
+router.get  ("/alltask", authenticate, allTask);
 
 export default router;
